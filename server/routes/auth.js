140c09ead7b7565cb05892069126cc099c7b01ca
Sakib <csakib049@gmail.com>
2026-07-23 12:48:17 +0600

    add the backend deployed link into the frontend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,6 @@ import { login, seedAdmin } from '../controllers/authController.js';
 const router = Router();
 
 router.post('/login', login);
-router.post('/seed', seedAdmin);
+router.all('/seed', seedAdmin);
 
 export default router;
